916f77c360f294f37e01e8c17770d1eda3b3a982
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2021-08-14 01:15:22 -0400

    Plugin Boilerplate set

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 const { Plugin } = require("powercord/entities");
 const { get } = require("powercord/http");
 
+module.exports = class valorantStats extends Plugin {
+    startPlugin(){
+        powercord.api.commands.registerCommand({
+            command: 'val',
+            description: 'Get Valorant Stats',
+            usage: '{c} [region] [name] [tag]',
+            executor: async (args) => {
+                console.log(args);
+            }
+        });    
+    }
+    pluginWillUnload() {
+        powercord.api.commands.unregisterCommand('val');
+    }
+};
