58fa6f4169ad9e357a854391f609795111eee1d1
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2021-08-14 01:17:59 -0400

    Argument length checking

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ module.exports = class valorantStats extends Plugin {
             usage: '{c} [region] [name] [tag]',
             executor: async (args) => {
                 console.log(args);
+
+                if (args.length < 1){
+                    console.log(args.length);
+                    return {
+                        username: "Valorant Stats",
+                        avatar_url: "https://www.freeiconspng.com/uploads/orange-error-icon-0.png",
+                        result: `Invalid arguments. Run \`${powercord.api.commands.prefix}val {Region} {Name} {Tag}\`.`
+                    };
+                }
+
+                if (args.length > 3){
+                    console.log(args.length);
+                    return {
+                        username: "Valorant Stats",
+                        avatar_url: "https://www.freeiconspng.com/uploads/orange-error-icon-0.png",
+                        result: `Replace any spaces in the username with an underscore!`
+                    };
+                }
+
+
             }
         });    
     }
